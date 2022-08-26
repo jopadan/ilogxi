9283383d26eb741562e32bdba3ff9de474d702b4
Jon Daniel <jondaniel879@gmail.com>
2022-08-26 20:28:55 +0200

    add __cplusplus support

--- ilogxi.h
+++ ilogxi.h
@@ -1,12 +1,14 @@
 #pragma once
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdint.h>
 #include <math.h>
 #include <x86gprintrin.h>
 #include <limits.h>
 
-extern "C"
-{
 	inline uint8_t ilogxid(int32_t i, uint32_t base)
 	{
 		return ((__bsrd(INT_MAX) - __lzcnt32(i)) / (__bsrd(UINT_MAX) - __lzcnt32(base)));
@@ -16,4 +18,7 @@ extern "C"
 	{
 		return ((__bsrq(INT64_MAX) - __lzcnt64(i)) / (__bsrq(UINT64_MAX) - __lzcnt64(base)));
 	}
+
+#ifdef __cplusplus
 }
+#endif
