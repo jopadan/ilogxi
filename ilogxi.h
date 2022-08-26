68077675a4f84f883b3816a4fb9c669d51fff972
Jon Daniel <jondaniel879@gmail.com>
2022-08-26 21:30:06 +0200

    assert base range

--- ilogxi.h
+++ ilogxi.h
@@ -8,14 +8,17 @@ extern "C" {
 #include <math.h>
 #include <x86gprintrin.h>
 #include <limits.h>
+#include <assert.h>
 
-	inline uint8_t ilogxid(int32_t i, uint32_t base)
+	inline uint8_t ilogxid(int32_t i, uint8_t base)
 	{
-		return ((__bsrd(INT_MAX) - __lzcnt32(i)) / (__bsrd(UINT_MAX) - __lzcnt32(base)));
+		assert(base > 1 && base < 32);
+		return ((__bsrd(INT32_MAX) - __lzcnt32(i)) / (__bsrd(UINT32_MAX) - __lzcnt32(base)));
 	}
 
-	inline uint8_t ilogxiq(int64_t i, uint64_t base)
+	inline uint8_t ilogxiq(int64_t i, uint8_t base)
 	{
+		assert(base > 1 && base < 64);
 		return ((__bsrq(INT64_MAX) - __lzcnt64(i)) / (__bsrq(UINT64_MAX) - __lzcnt64(base)));
 	}
 
