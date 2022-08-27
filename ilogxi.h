c8ebe0af10e217e2ed09eac268857d6e54bb0fe6
Jon Daniel <jondaniel879@gmail.com>
2022-08-27 02:18:36 +0200

    add generic lzcnt and bsr

--- ilogxi.h
+++ ilogxi.h
@@ -6,20 +6,27 @@ extern "C" {
 
 #include <stdint.h>
 #include <math.h>
-#include <x86gprintrin.h>
 #include <limits.h>
 #include <assert.h>
 
-	inline uint8_t ilogxid(int32_t i, uint8_t base)
+#ifdef __i386__ || __x86_64__
+#include <x86gprintrin.h>
+#endif
+
+#ifndef __LZCNT__
+#include "lzcnt.h"
+#endif
+
+	inline uint8_t ilogxid(int32_t i, uint32_t base)
 	{
 		assert(base > 1 && base < 32);
-		return ((__bsrd(INT32_MAX) - __lzcnt32(i)) / (__bsrd(UINT32_MAX) - __lzcnt32(base)));
+			return ((__bsrd(INT32_MAX) - __lzcnt32(i)) / (__bsrd(UINT32_MAX) - __lzcnt32(base)));
 	}
 
-	inline uint8_t ilogxiq(int64_t i, uint8_t base)
+	inline uint8_t ilogxiq(int64_t i, uint64_t base)
 	{
 		assert(base > 1 && base < 64);
-		return ((__bsrq(INT64_MAX) - __lzcnt64(i)) / (__bsrq(UINT64_MAX) - __lzcnt64(base)));
+			return ((__bsrq(INT64_MAX) - __lzcnt64(i)) / (__bsrq(UINT64_MAX) - __lzcnt64(base)));
 	}
 
 #ifdef __cplusplus
---
+++ lzcnt.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <stdint.h>
+
+uint32_t lzcnt32_generic(uint32_t x);
+uint32_t lzcnt64_generic(uint64_t x);
+
+inline int32_t bsrd_generic(int32_t x)
+{
+	if(x == 0)
+		return -1;
+	else
+		return ((INT32_WIDTH - 1) - lzcnt32_generic(x));
+}
+
+inline int64_t bsrq_generic(int64_t x)
+{
+	if(x == 0)
+		return -1;
+	else
+		return ((INT64_WIDTH - 1) - lzcnt64_generic(x));
+}
+
+#undef __lzcnt32
+#define __lzcnt32 lzcnt32_generic
+
+#undef __lzcnt64
+#define __lzcnt64 lzcnt64_generic
+
+#undef __bsrd
+#define __bsrd bsrd_generic
+
+#undef __bsrq
+#define __bsrq bsrq_generic
+
