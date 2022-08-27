2be8362c4120959389f1d1da1398c219d022d5b5
Jon Daniel <jondaniel879@gmail.com>
2022-08-27 15:43:32 +0200

    remove bsr

--- ilogxi.h
+++ ilogxi.h
@@ -17,16 +17,16 @@ extern "C" {
 #include "lzcnt.h"
 #endif
 
-	inline uint8_t ilogxid(int32_t i, uint32_t base)
+	inline uint8_t ilogxid(int32_t i, int32_t base)
 	{
 		assert(base > 1 && base < 32);
-			return ((__bsrd(INT32_MAX) - __lzcnt32(i)) / (__bsrd(UINT32_MAX) - __lzcnt32(base)));
+			return (UINT32_WIDTH - __lzcnt32(i)) / (UINT32_WIDTH - __lzcnt32(base)));
 	}
 
-	inline uint8_t ilogxiq(int64_t i, uint64_t base)
+	inline uint8_t ilogxiq(int64_t i, int64_t base)
 	{
 		assert(base > 1 && base < 64);
-			return ((__bsrq(INT64_MAX) - __lzcnt64(i)) / (__bsrq(UINT64_MAX) - __lzcnt64(base)));
+			return ((UINT64_WIDTH - __lzcnt64(i)) / (UINT64_WIDTH - __lzcnt64(base)));
 	}
 
 #ifdef __cplusplus
