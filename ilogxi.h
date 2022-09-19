63e991832d386240af11a4b662c89cbdcf5fe84e
Jon Daniel <jondaniel879@gmail.com>
2022-09-19 19:04:52 +0200

    remove lzcnt for now and use builtin_clz

--- ilogxi.h
+++ ilogxi.h
@@ -13,21 +13,33 @@ extern "C" {
 #include <x86gprintrin.h>
 #endif
 
-#ifndef __LZCNT__
-#include "lzcnt.h"
-#endif
+extern inline int8_t ilogxid(int32_t i, int8_t base)
+{
+	assert(base > 1 && base < 32);
+	#if __has_builtin(__builtin_clz) || defined(__GNUC__)
+		return ((31 - __builtin_clz(i)) / (31 - __builtin_clz(base)));
+	#else
+		#error "__builtin_clz instruction not supported!"
+	#endif
+}
 
-	inline uint8_t ilogxid(int32_t i, int32_t base)
-	{
-		assert(base > 1 && base < 32);
-			return (UINT32_WIDTH - __lzcnt32(i)) / (UINT32_WIDTH - __lzcnt32(base)));
-	}
-
-	inline uint8_t ilogxiq(int64_t i, int64_t base)
-	{
-		assert(base > 1 && base < 64);
-			return ((UINT64_WIDTH - __lzcnt64(i)) / (UINT64_WIDTH - __lzcnt64(base)));
-	}
+extern inline int8_t ilogxiq(int64_t i, int8_t base)
+{
+	assert(base > 1 && base < 64);
+	#if __WORDSIZE == 64
+		#if __has_builtin(__builtin_clzl) || defined(__GNUC__)
+		return ((63 - __builtin_clzl(i)) / (63 - __builtin_clzl(base)));
+		#else
+			#error "__builtin_clzl instruction not supported!"
+		#endif
+	#else
+		#if __has_builtin(__builtin_clzll) || defined(__GNUC__)
+		return ((63 - __builtin_clzll(i)) / (63 - __builtin_clzll(base)));
+		#else
+			#error "__builtin_clzll instruction not supported!"
+		#endif
+	#endif
+}
 
 #ifdef __cplusplus
 }
